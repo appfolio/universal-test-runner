2f2ea22724a57f0f0828a14095bc803246317a98
Otto <af-otto@users.noreply.github.com>
2023-03-14 23:55:38 +0000

    chore: update for fork compatibility

--- packages/universal-test-adapter-maven/src/log.ts
+++ packages/universal-test-adapter-maven/src/log.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { makeLogger } from '@aws/universal-test-runner-logger'
+import { makeLogger } from '@appfolio/universal-test-runner-logger'
 
 const LOG_PREFIX = '[universal-test-adapter-maven]:'
 
--- packages/universal-test-runner/src/readProtocol.ts
+++ packages/universal-test-runner/src/readProtocol.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ProtocolResult } from '@aws/universal-test-runner-types'
+import { ProtocolResult } from '@appfolio/universal-test-runner-types'
 import { mapEnvToResult, Environment } from './mapEnvToResult'
 import { log } from './log'
 import { ProtocolEnvVars } from './ProtocolEnvVars'
